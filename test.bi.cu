efde1893341451c61159c212775d553a0785d7ad
Akhil Guliani <guliani@cs.wisc.edu>
2020-02-16 13:36:36 -0600

    Partial all to all broadcast benchmark

--- test.bi.cu
+++ test.bi.cu
@@ -9,23 +9,28 @@
 #include <cmath>
 
 // Randomization helpers 
-void init_sin(float *A, size_t M, size_t N, size_t lda, size_t stride = 0, size_t batch_count = 1){
+// adapted from https://github.com/ROCmSoftwarePlatform/rocBLAS/blob/rocm-3.0/clients/include/rocblas_init.hpp#L42
 
-    for(size_t i_batch = 0; i_batch < batch_count; i_batch++)
-        for(size_t i = 0; i < M; ++i)
-            for(size_t j = 0; j < N; ++j)
-                A[i + j * lda + i_batch * stride] = sin(i + j * lda + i_batch * stride);
+void spin_loop(){
+	unsigned long long loop = (unsigned long)-1;
+	unsigned long long sum = 0;
+	
+	for (unsigned long long i=0; i < loop; i++){
+		sum = sum + i;
+	}
+}
 
+void init_sin(float *A, size_t nr_rows_A, size_t nr_cols_A){
+    for(size_t i = 0; i < nr_rows_A; ++i)
+        for(size_t j = 0; j < nr_cols_A; ++j)
+	    A[i + j * nr_rows_A] = sin(i + j * nr_rows_A);
 }
 
 
-void init_cos(float *A, size_t M, size_t N, size_t lda, size_t stride = 0, size_t batch_count = 1){
-
-    for(size_t i_batch = 0; i_batch < batch_count; i_batch++)
-        for(size_t i = 0; i < M; ++i)
-            for(size_t j = 0; j < N; ++j)
-                A[i + j * lda + i_batch * stride] = cos(i + j * lda + i_batch * stride);
-
+void init_cos(float *A, size_t nr_rows_A, size_t nr_cols_A){
+    for(size_t i = 0; i < nr_rows_A; ++i)
+        for(size_t j = 0; j < nr_cols_A; ++j)
+	    A[i + j * nr_rows_A] = cos(i + j * nr_rows_A);
 }
 
 //Print matrix A(nr_rows_A, nr_cols_A) storage in column-major format
@@ -49,12 +54,14 @@ void print_matrix(const float *A, int nr_rows_A, int nr_cols_A) {
  }                                                                 \
 }
 
+// Function to enable NVLINK between card pairs 
+void setup_nvlink(int numGPUs){
+	if (numGPUs >= 4){
+	    numGPUs = 3;
+	}
 
-void setup_nvlink(){
-	int numGPUs = 4;
-
-	int i = 0;
-	for (int j = 0; j < numGPUs; j++) {
+	for (int i = 0; i <= numGPUs; i++) {
+	    for (int j = i+1; j <= numGPUs; j++) {
 		int access = 0;
 		cudaDeviceCanAccessPeer(&access, i, j);
 		if (access) {
@@ -71,6 +78,7 @@ void setup_nvlink(){
 			cudaCheckError();
 		}
 		fflush(stdout);
+	    }
 	}
 }
 
@@ -89,7 +97,8 @@ int main(int argc, char* argv[]) {
 	
 	int nlinks = atoi(argv[3]);
         
-        setup_nvlink();
+        setup_nvlink(nlinks);
+	// spin_loop();
 
 	cudaSetDevice(0);
 	cudaStream_t copyStream, copyStream2;
@@ -109,12 +118,13 @@ int main(int argc, char* argv[]) {
 	cudaMallocHost((void**) &src_h, SIZE * sizeof(int));
 
 	for (int i = 0; i < SIZE ; ++i) {
-		src[i] = (unsigned int)-1;
-		src_h[i] = 4;
+		// src[i] = (unsigned int)-1;
+		src[i] = sin(i);
+		src_h[i] = cos(i);
 	}
 
 	// Allocate DST on gpu	
-	int* dst;
+	int* dst, dst_1, dst_2, dst_3;
 	cudaMalloc(&dst, SIZE*sizeof(int));
 
 	cudaMemcpyAsync((void*)dst, (void*)src, sizeof(int)*SIZE , cudaMemcpyHostToDevice, copyStream);
@@ -144,16 +154,26 @@ int main(int argc, char* argv[]) {
 	cudaSetDevice(0);
 
 	cudaDeviceSynchronize();
-	
+	// cudaMemcpyAsync((void*)src, (void*)dst, sizeof(int)*SIZE , cudaMemcpyDeviceToHost, copyStream);
+        printf("START\n");
 	for (int i = 0; i < reps; i++) {
-	    cudaMemcpyAsync((void*)src, (void*)dst, sizeof(int)*SIZE , cudaMemcpyDeviceToHost, copyStream);
-	    if (nlinks >=1)  
-	    	cudaMemcpyAsync((void*)src_1, (void*)dst, sizeof(int)*SIZE , cudaMemcpyDeviceToHost, copyStream2);
-	    if (nlinks >= 2)
-	    	cudaMemcpyAsync((void*)src_2, (void*)dst, sizeof(int)*SIZE , cudaMemcpyDeviceToHost, copyStream3);
-	    if (nlinks >= 3)
-	    	cudaMemcpyAsync((void*)src_3, (void*)dst, sizeof(int)*SIZE , cudaMemcpyDeviceToHost, copyStream4);
-        }
+            for (int j =0; j < nlinks; j++){ 
+	    // cudaMemcpyAsync((void*)src, (void*)dst, sizeof(int)*SIZE , cudaMemcpyDeviceToDevice, copyStream);
+	        if (j == 0) {
+		   cudaSetDevice(j);
+		   if (nlinks >=1)  
+	    	       cudaMemcpyAsync((void*)src_1, (void*)dst, sizeof(int)*SIZE , cudaMemcpyDeviceToDevice, copyStream2);
+	           if (nlinks >= 2)
+	    	       cudaMemcpyAsync((void*)src_2, (void*)dst, sizeof(int)*SIZE , cudaMemcpyDeviceToDevice, copyStream3);
+	           if (nlinks >= 3)
+	    	       cudaMemcpyAsync((void*)src_3, (void*)dst, sizeof(int)*SIZE , cudaMemcpyDeviceToDevice, copyStream4);
+		}
+		if (j == 1){
+		   cudaSetDevice(j)
+		}
+		
+	    }        
+	}
 
 	// Create a handle for CUBLAS	        
 	cudaStreamSynchronize(copyStream);
