aea39e4cd5fa36a510bb04dbc2cb1f6d1a92f1f4
Akhil Guliani <guliani@cs.wisc.edu>
2020-02-16 19:43:33 -0600

    The overlapped test current setup

--- test.2.cu
+++ test.2.cu
@@ -2,6 +2,7 @@
 // C(m,n) = A(m,k) * B(k,n)
 #include <iostream>
 #include <cstdlib>
+#include <cstdio>
 #include <ctime>
 #include <cublas_v2.h>
 #include <curand.h>
@@ -55,6 +56,41 @@ static const char *_cudaGetErrorEnum(cublasStatus_t error)
 }
 #endif
 
+//Macro for checking cuda errors following a cuda launch or api call
+#define cudaCheckError() {                                          \
+	cudaError_t e=cudaGetLastError();                                 \
+	if(e!=cudaSuccess) {                                              \
+		printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e));           \
+		exit(0); \
+	}                                                                 \
+}
+
+
+void setup_nvlink(){
+	int numGPUs = 4;
+
+	int i = 0;
+	for (int j = 0; j < numGPUs; j++) {
+		int access = 0;
+		cudaDeviceCanAccessPeer(&access, i, j);
+		if (access) {
+			printf("Enabling %d to %d\n", i, j);
+			cudaSetDevice(i);
+			cudaCheckError();
+			cudaDeviceEnablePeerAccess(j, 0);
+			cudaCheckError();
+			cudaSetDevice(j);
+			cudaCheckError();
+			cudaDeviceEnablePeerAccess(i, 0);
+			cudaCheckError();
+			cudaSetDevice(i);
+			cudaCheckError();
+		}
+		fflush(stdout);
+	}
+}
+
+
 // Multiply the arrays A and B on GPU and save the result in C
 // C(m,n) = A(m,k) * B(k,n)
 void gpu_blas_mmul( cublasHandle_t handle, const float *A, const float *B, float *C, const int m, const int k, const int n) {
@@ -66,7 +102,7 @@ void gpu_blas_mmul( cublasHandle_t handle, const float *A, const float *B, float
 
 	// Do the actual multiplication
 	cublasStatus_t err = cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
-  	if (err != CUBLAS_STATUS_SUCCESS)
+	if (err != CUBLAS_STATUS_SUCCESS)
 		std::cout << "Error: " <<  _cudaGetErrorEnum(err) << std::endl;
 
 }
@@ -86,20 +122,28 @@ void print_matrix(const float *A, int nr_rows_A, int nr_cols_A) {
 
 int main(int argc, char* argv[]) {
 
-	if (argc != 3){
-		std::cout << "USAGE: " << argv[0] <<" <size> <inner-reps>" <<std::endl ;
+	if (argc != 4){
+		std::cout << "USAGE: " << argv[0] <<" <size> <inner-reps> nlinks" <<std::endl ;
 		exit(-1);
 	}
 	int size = atoi(argv[1]);
 	int reps = atoi(argv[2]);
+	int nlinks = atoi(argv[3]);
+
+	setup_nvlink();
 
 	cudaStream_t computeStream;
 	cudaError_t result;
-	result = cudaStreamCreate(&computeStream);
+
+	result = cudaStreamCreateWithFlags(&computeStream, cudaStreamNonBlocking);
 
 	cudaStream_t copyStream, copyStream2;
-	result = cudaStreamCreate(&copyStream);
-	result = cudaStreamCreate(&copyStream2);
+	cudaStream_t copyStream3, copyStream4;
+	result = cudaStreamCreateWithFlags(&copyStream, cudaStreamNonBlocking);
+	result = cudaStreamCreateWithFlags(&copyStream2, cudaStreamNonBlocking);
+	result = cudaStreamCreateWithFlags(&copyStream3, cudaStreamNonBlocking);
+	result = cudaStreamCreateWithFlags(&copyStream4, cudaStreamNonBlocking);
+
 
 
 	// Allocate the src on CPU
@@ -114,9 +158,38 @@ int main(int argc, char* argv[]) {
 		dest_h[i] = 1;
 	}
 
+	cudaSetDevice(0);
 	// Allocate DST on gpu	
 	int* dst;
 	cudaMalloc(&dst, SIZE*sizeof(int));
+
+	// Allocate buffers on all cpus:
+	cudaSetDevice(1);
+	int* src_1;
+	cudaMalloc(&src_1, SIZE*sizeof(int));
+
+	cudaMemcpy((void*)src_1, (void*)src, sizeof(int)*SIZE , cudaMemcpyHostToDevice);
+	cudaDeviceSynchronize();
+
+	cudaSetDevice(2);
+	int* src_2;
+	cudaMalloc(&src_2, SIZE*sizeof(int));
+
+	cudaMemcpy((void*)src_2, (void*)src, sizeof(int)*SIZE , cudaMemcpyHostToDevice);
+	cudaDeviceSynchronize();
+
+	cudaSetDevice(3);
+	int* src_3;
+	cudaMalloc(&src_3, SIZE*sizeof(int));
+
+	cudaMemcpy((void*)src_3, (void*)src, sizeof(int)*SIZE , cudaMemcpyHostToDevice);
+	cudaDeviceSynchronize();
+
+
+	cudaSetDevice(0);
+
+	cudaDeviceSynchronize();
+
 
 
 	// Allocate 3 arrays on CPU
@@ -156,7 +229,7 @@ int main(int argc, char* argv[]) {
 	cublasHandle_t handle;
 	cublasCreate(&handle);
 	cublasSetStream(handle, computeStream);
-	
+
 	cudaDeviceSynchronize();
 
 	for (int j = 0 ; j < 100; j++){
@@ -164,15 +237,28 @@ int main(int argc, char* argv[]) {
 		gpu_blas_mmul(handle, d_A, d_B, d_C, nr_rows_A, nr_cols_A, nr_cols_B);
 		for (int i=0; i< reps; i++){
 			// each stable copy takes about 162 miliseconds
-			cudaMemcpyAsync((void*)src, (void*)dst, sizeof(int) * SIZE, cudaMemcpyDeviceToHost, copyStream);
+			// cudaMemcpyAsync((void*)src, (void*)dst, sizeof(int) * SIZE, cudaMemcpyDeviceToHost, copyStream);
 			// cudaMemcpyAsync((void*)dest_h, (void*)dst, sizeof(int) * SIZE, cudaMemcpyDeviceToHost, copyStream2);
+			if (nlinks >=1)  
+				cudaMemcpyAsync((void*)src_1, (void*)dst, sizeof(int)*SIZE , cudaMemcpyDeviceToDevice, copyStream2);
+			if (nlinks >= 2)
+				cudaMemcpyAsync((void*)src_2, (void*)dst, sizeof(int)*SIZE , cudaMemcpyDeviceToDevice, copyStream3);
+			if (nlinks >= 3)
+				cudaMemcpyAsync((void*)src_3, (void*)dst, sizeof(int)*SIZE , cudaMemcpyDeviceToDevice, copyStream4);
+
 		}
-        	cudaStreamSynchronize(copyStream);
-		// cudaStreamSynchronize(copyStream2);
+		// Create a handle for CUBLAS	        
+		cudaStreamSynchronize(copyStream);
+		cudaStreamSynchronize(copyStream2);
+		cudaStreamSynchronize(copyStream3);
+		cudaStreamSynchronize(copyStream4);
+
 	}
 	cudaStreamSynchronize(computeStream);  
 	cudaStreamSynchronize(copyStream);
-	// cudaStreamSynchronize(copyStream2);
+	cudaStreamSynchronize(copyStream2);
+	cudaStreamSynchronize(copyStream3);
+	cudaStreamSynchronize(copyStream4);
 
 	// Destroy the handle
 	cublasDestroy(handle);
@@ -187,7 +273,25 @@ int main(int argc, char* argv[]) {
 	cudaFree(d_B);
 	cudaFree(d_C);
 
+	cudaFreeHost(src);
+	cudaFreeHost(dest_h);
+	cudaFree(dst);	
+
+	cudaSetDevice(1);
+	cudaFree(src_1);
+
+	cudaSetDevice(2);
+	cudaFree(src_2);
+
+	cudaSetDevice(3);
+	cudaFree(src_3);
+
+
 	result = cudaStreamDestroy(computeStream);
+	result = cudaStreamDestroy(copyStream);
+	result = cudaStreamDestroy(copyStream2);
+	result = cudaStreamDestroy(copyStream3);
+	result = cudaStreamDestroy(copyStream4);
 
 	// Free CPU memory
 	free(h_A);
